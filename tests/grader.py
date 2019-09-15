7c9093776a1870332b94a708863976ea5b3b73d2
profknight <ahknight@pipeline.sbcc.edu>
2019-09-14 17:59:37 -0700

    Updated to add tests and remove unnecessary files

--- tests/grader.py
+++ tests/grader.py
@@ -33,27 +33,28 @@ class CelsiusToFahrenheitTest(unittest.TestCase):
         print("comes from grading related to documentation, algorithms, and other")
         print("criteria.\n\n")
 
-    def executeTest(self, c, f):
+    def executeTest(self, c, expected, regex):
         # call hello world script command ##
         p = subprocess.Popen("python3 ../src/celsius_to_fahrenheit.py", stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True, bufsize=1)
 
         (output, err) = p.communicate('{0}'.format(c).encode())
         p_status = p.wait()
 
-        self.assertTrue(re.search(f, output.decode('utf-8')) is not None)
+        m = re.search(regex, output.decode('utf-8'))
+        self.assertEqual(expected, float(m.group(1)))
         CelsiusToFahrenheitTest.testScore += 4
         self.assertEqual(0, p_status)
         CelsiusToFahrenheitTest.testScore += (2 + 2/3)
 
     def testFrozen(self):
-        self.executeTest(0, "\D*32.0\D?.*\n$")
+        self.executeTest(0, 32, "\D*(\d+).*\n$")
 
     def testBoiling(self):
-        self.executeTest(100, "\D*212.0\D?.*\n$")
+        self.executeTest(100, 212, "\D*(\d+).*\n$")
 
     def testRandom(self):
         c = random.randint(-12, 200)
-        self.executeTest(c, '\D*{0}.*\D?\n$'.format(9/5 * c + 32))
+        self.executeTest(c, 9/5 * c + 32, '\D*(\d+(\.\d+)?).*\n$')
 
 
 if __name__ == '__main__':
